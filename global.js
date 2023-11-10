96a06b9b03994f524755854417ff2d4cad5847c4
Gannon2 <gstephens27@ivytech.edu>
2023-11-10 11:40:39 -0500

    M03 Tutorial V1

---
+++ global.js
@@ -0,0 +1,22 @@
+const speak = () => {
+    console.log('hello, ninjas!');
+  }
+  
+  speak();
+  
+  // console.log(global);
+  
+  setTimeout(() => {
+    console.log('in the timeout');
+    clearInterval(int);
+  }, 3000);
+  
+  const int = setInterval(() => {
+    console.log('in the interval');
+  }, 1000);
+  
+  console.log(__dirname);
+  console.log(__filename);
+  
+  // no access to DOM methods
+  console.log(document.querySelector);
